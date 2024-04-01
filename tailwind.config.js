e25ae2c9a72e2e8c18f8ab9fed8ea7dfe6ca4580
Flávia Reis <flaviarster@gmail.com>
2024-04-01 09:02:54 -0300

    Added custom colors and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Inter': ['Inter', 'sans-serif'],
+      },
+      colors: {
+        blue: '#345FF6',
+        gunmetal: '#253347',
+        dark_eletric_blue: '#5E6E85',
+        borders: '#D8E2E7',
+        pure_white: '#FFFFFF',
+      },
+    },
   },
   plugins: [],
 }
