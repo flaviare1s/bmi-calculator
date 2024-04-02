49539fb883ca17c3054c8d652e3dae7da17bb1f5
Flávia Reis <flaviarster@gmail.com>
2024-04-02 17:58:32 -0300

    Working on script

--- src/script.js
+++ src/script.js
@@ -25,7 +25,9 @@ metricButton.addEventListener('click', showMetricForm)
 imperialButton.addEventListener('click', showImperialForm)
 
 
-// Validação dos inputs no sitema métrico:
+
+// SISTEMA METRIC:
+// Validação dos inputs no sitema metric:
 const heightMetricInput = document.querySelector('#height')
 const weightMetricInput = document.querySelector('#weight')
 
@@ -55,7 +57,7 @@ function validateMetricInputs() {
 heightMetricInput.addEventListener('input', validateMetricInputs)
 weightMetricInput.addEventListener('input', validateMetricInputs)
 
-// Cálculo do IMC no sistema métrico:
+// Cálculo do IMC no sistema metric:
 function bmiCalculationMetric() {
     if (!validateMetricInputs()) {
         return
@@ -70,12 +72,68 @@ function bmiCalculationMetric() {
 
     result.innerText = bmi.toFixed(1)
     classificationMetric(bmi)
+    rangeMetric(height)
 }
 heightMetricInput.addEventListener('input', bmiCalculationMetric)
 weightMetricInput.addEventListener('input', bmiCalculationMetric)
 
+// Classificações no sistema metric:
+const classificationSpan = document.querySelector('#classification')
+
+function classificationMetric(bmi) {
+    if(bmi <= 16) {
+        classificationSpan.innerText = 'severe thinnes'
+    } else if(bmi > 16 && bmi <= 17 ) {
+        classificationSpan.innerText = 'moderate thinnes'
+    } else if(bmi > 17 && bmi <= 18.5 ) {
+        classificationSpan.innerText = 'mild thinnes'
+    }  else if(bmi > 18.5 && bmi <= 25 ) {
+        classificationSpan.innerText = 'healthy weight'
+    }  else if(bmi > 25 && bmi <= 30 ) {
+        classificationSpan.innerText = 'overweight'
+    }  else if(bmi > 30 && bmi <= 35 ) {
+        classificationSpan.innerText = 'obese class I'
+    }  else if(bmi > 35 && bmi <= 40 ) {
+        classificationSpan.innerText = 'obese class II'
+    }  else {
+        classificationSpan.innerText = 'obese class III'
+    }     
+}
+
+// Range no sitema metric:
+const rangeSpan = document.querySelector('#range')
 
 
+function rangeMetric(height) {
+    const heightCm = height * 100
+    if(heightCm > 150 && heightCm <= 155) {
+        rangeSpan.innerText = '41.6kg - 56kg'
+    } else if(heightCm > 155 && heightCm <= 160 ) {
+        rangeSpan.innerText = '44.4kg - 59.8kg'
+    } else if(heightCm > 160 && heightCm <= 165 ) {
+        rangeSpan.innerText = '47.4kg - 63.7kg'
+    }  else if(heightCm > 165 && heightCm <= 170 ) {
+        rangeSpan.innerText = '50.4kg - 67.8kg'
+    }  else if(heightCm > 170 && heightCm <= 175 ) {
+        rangeSpan.innerText = '53.5.6kg - 72kg'
+    }  else if(heightCm > 175 && heightCm <= 180 ) {
+        rangeSpan.innerText = '56.7kg - 76.3kg'
+    }  else if(heightCm > 180 && heightCm <= 185 ) {
+        rangeSpan.innerText = '59.9kg - 80.7kg'
+    }  else if(heightCm > 185) {
+        rangeSpan.innerText = '63.3kg - 85.2kg'
+    }     
+}
+
+
+
+
+
+
+
+
+
+// SISTEMA IMPERIAL:
 // Validação dos inputs no sitema imperial:
 const feetInput = document.querySelector('#ft')
 const inchesInput = document.querySelector('#in')
@@ -156,30 +214,7 @@ inchesInput.addEventListener('input', bmiCalculationImperial)
 stoneInput.addEventListener('input', bmiCalculationImperial)
 poundsInput.addEventListener('input', bmiCalculationImperial)
 
-
-// Classificações:
-const classificationSpan = document.querySelector('#classification')
-
-function classificationMetric(bmi) {
-    if(bmi <= 16) {
-        classificationSpan.innerText = 'severe thinnes'
-    } else if(bmi > 16 && bmi <= 17 ) {
-        classificationSpan.innerText = 'moderate thinnes'
-    } else if(bmi > 17 && bmi <= 18.5 ) {
-        classificationSpan.innerText = 'mild thinnes'
-    }  else if(bmi > 18.5 && bmi <= 25 ) {
-        classificationSpan.innerText = 'healthy weight'
-    }  else if(bmi > 25 && bmi <= 30 ) {
-        classificationSpan.innerText = 'overweight'
-    }  else if(bmi > 30 && bmi <= 35 ) {
-        classificationSpan.innerText = 'obese class I'
-    }  else if(bmi > 35 && bmi <= 40 ) {
-        classificationSpan.innerText = 'obese class II'
-    }  else {
-        classificationSpan.innerText = 'obese class III'
-    }     
-}
-
+// Classificações no sistema imperial:
 function classificationImperial(bmiImperial) {
     if(bmiImperial <= 16) {
         classificationSpan.innerText = 'severe thinnes'
@@ -199,14 +234,5 @@ function classificationImperial(bmiImperial) {
         classificationSpan.innerText = 'obese class III'
     }     
 }
-
-
-
-// Range:
-const rangeSpan = document.querySelector('#range')
-
-
-
-
 
 
